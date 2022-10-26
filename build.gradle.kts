495ad14e36be795133526e609c6acf1f2d079cf9
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2022-10-26 13:41:18 +0200

    Dependency bumps (#122)
    * Dependency bumps
    
    * Syntax
    
    * Fiks build.gradle.kts
    
    * Fjernet jakarta.el

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,28 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val mainClass = "no.nav.helse.AleneomsorgProsesseringKt"
-
-val dusseldorfKtorVersion = "3.2.1.1-15e2e63"
-val k9FormatVersion = "6.1.5"
+val dusseldorfKtorVersion = "3.2.1.2-93aa998"
+val ktorVersion = ext.get("ktorVersion").toString()
+val slf4jVersion = ext.get("slf4jVersion").toString()
 val kotlinxCoroutinesVersion = ext.get("kotlinxCoroutinesVersion").toString()
+
+val k9FormatVersion = "7.0.4"
+
 val kafkaEmbeddedEnvVersion = ext.get("kafkaEmbeddedEnvVersion").toString()
 val kafkaVersion = ext.get("kafkaVersion").toString() // Alligned med version fra kafka-embedded-env
-val slf4jVersion = ext.get("slf4jVersion").toString()
-val ktorVersion = ext.get("ktorVersion").toString()
 val openhtmltopdfVersion = "1.0.10"
-val handlebarsVersion = "4.3.0"
+val handlebarsVersion = "4.3.1"
 val fuelVersion = "2.3.1"
 
+val mainClass = "no.nav.helse.AleneomsorgProsesseringKt"
+
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 buildscript {
     // Henter ut diverse dependency versjoner, i.e. ktorVersion.
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/b18c5feeca2840e6812eb805d50937d7aa0aca6a/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/3bd7664b0352f77c0d6c124d1d03c57bc0284690/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
@@ -55,12 +57,16 @@ dependencies {
 
     // Test
     testImplementation("org.apache.kafka:kafka-clients:$kafkaVersion")
-    testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvVersion")
+    testImplementation("no.nav:kafka-embedded-env:$kafkaEmbeddedEnvVersion") {
+        exclude("org.glassfish", "jakarta.el")
+        exclude("org.apache.kafka", "kafka-clients")
+        exclude("javax.el", "javax.el-api")
+    }
     testImplementation("no.nav.helse:dusseldorf-test-support:$dusseldorfKtorVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
         exclude(group = "org.eclipse.jetty")
     }
-    testImplementation("org.skyscreamer:jsonassert:1.5.0")
+    testImplementation("org.skyscreamer:jsonassert:1.5.1")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 }
 
